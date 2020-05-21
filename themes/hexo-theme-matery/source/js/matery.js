02fa8076bd6122ea724f66687cb955f1a8bf5e86
zhongshiyi <1351896179@qq.com>
2020-05-21 09:14:17 +0800

    再次修改为图片懒加载

--- themes/hexo-theme-matery/source/js/matery.js
+++ themes/hexo-theme-matery/source/js/matery.js
@@ -104,13 +104,13 @@ $(function () {
             selector: '.img-item',
             // 启用字幕
             subHtmlSelectorRelative: true,
-            // showThumbByDefault: false //这句加上
+            showThumbByDefault: false //这句加上
             
         });
 
-        // $(document).find('img[data-original]').each(function () {
-        //     $(this).parent().attr("href", $(this).attr("data-original"));
-        // });
+        $(document).find('img[data-original]').each(function () {
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
